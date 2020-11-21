41507f0ddf14a3b1cd26577b27e40b9b1eab6bfa
SEAN-KWEI <chen.sean.608@gmail.com>
2020-11-21 22:43:41 +0800

    edited landing

--- resources/js/spin-box.js
+++ resources/js/spin-box.js
@@ -2,6 +2,7 @@
 // SEAN CHEN
 
 var img;
+var fc = 0;
 
 function preload() {
     // rotate(radians(180));
@@ -11,17 +12,27 @@ function preload() {
 function setup() {
     createCanvas(200, 200, WEBGL);   
     createCanvas(200, 200);
+    textSize(width / 3);
+    textAlign(CENTER, CENTER);
     ortho(-width/2,width/2,height/2,-height/2,0,500); 
 }
 
 function draw() {
     background(255, 0.0);
     circle(0, 0, 100);
-    rotateY(frameCount * 0.01);
-    rotateX(frameCount * -0.01);
-    rotateZ(frameCount * 0.01);
-    rotateZ(0.01);
+    if (width/2 < mouseX && mouseX < width/2+150 && 
+        height/2 < mouseY && mouseY < height/2+150) {
+        fc ++;
+        rotateY(fc * 0.01);
+        rotateX(fc * -0.01);
+        rotateZ(fc * 0.01);
+    } else {
+        fc += 2.5;
+        rotateY(fc * 0.01);
+        rotateX(fc * -0.01);
+        rotateZ(fc * 0.01);
+    }
     texture(img);
-    orbitControl();
     box(100);
+    text('hello', 0, 50);
 }
